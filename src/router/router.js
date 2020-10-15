049e2c1c10cbe9e8719ef8894a53a84316748ae1
moccay <jutian88@qq.com>
2020-10-15 14:26:44 +0800

    注入列表

--- src/router/router.js
+++ src/router/router.js
@@ -1,15 +1,16 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "../views/home/home";
+import Vue from 'vue'
+import Router from 'vue-router'
+// import Home from "../views/home/home";
+import verifyList from '../views/verify-list'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: Home
-    },
+      path: '/',
+      name: 'verifyList',
+      component: verifyList
+    }
   ]
-});
+})
---
+++ src/views/verify-list/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <main class="main">
+    <left-table class="left-wrap" />
+    <right-table class="right-wrap" />
+  </main>
+</template>
+
+<script>
+import leftTable from './left-table'
+import rightTable from './right-table'
+export default {
+  components: { leftTable, rightTable },
+  data() {
+    return {
+      list: []
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+.main {
+  display: flex;
+  flex-wrap: nowrap;
+}
+.left-wrap,
+.right-wrap {
+  width: 50%;
+}
+</style>
